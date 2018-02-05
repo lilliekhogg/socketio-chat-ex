c900698b90e9593b61acba4d3211ce84433d65ff
lilliekhogg <lilliekhogg@gmail.com>
2018-02-05 21:20:18 +0000

    Create socket.io.js

---
+++ socket.io/socket.io.js
@@ -0,0 +1,3 @@
+!function(t,e){"object"==typeof exports&&"object"==typeof module?module.exports=e():"function"==typeof define&&define.amd?define([],e):"object"==typeof exports?exports.io=e():t.io=e()}(this,functi$
+}catch(s){}}}).call(e,function(){return this}())},function(t,e){try{t.exports="undefined"!=typeof XMLHttpRequest&&"withCredentials"in new XMLHttpRequest}catch(n){t.exports=!1}},function(t,e,n){(fu$
+//# sourceMappingURL=socket.io.js.map
