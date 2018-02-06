555a5a180b692d7061df3e1c1b59eb86b4c1e31c
lilliekhogg <lilliekhogg@gmail.com>
2018-02-06 11:50:03 +0000

    Update main.js

--- files/main.js
+++ files/main.js
@@ -227,7 +227,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to your game chat – ";
     log(message, {
       prepend: true
     });
